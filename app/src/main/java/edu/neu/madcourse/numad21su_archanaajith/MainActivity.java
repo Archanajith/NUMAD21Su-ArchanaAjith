38183c4d022eb09817874dd45dd72653ae145168
Archanajith <ajith.a@northeastern.edu>
2021-05-29 19:12:13 -0400

    Made changes to Assignment1, fixed About Me button

--- app/src/main/java/edu/neu/madcourse/numad21su_archanaajith/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21su_archanaajith/MainActivity.java
@@ -2,22 +2,48 @@ package edu.neu.madcourse.numad21su_archanaajith;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.nio.charset.Charset;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button aboutButton;
+    Button clickButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aboutButton = findViewById(R.id.button);
+        aboutButton.setOnClickListener(this);
+        clickButton = findViewById(R.id.button2);
+        clickButton.setOnClickListener(this);
     }
 
-    public void onClick(View view) {
-        TextView text=findViewById(R.id.textView3);
-        text.setText(" Name : Archana Ajith");
-        TextView textEmail=findViewById(R.id.textView5);
-        textEmail.setText(" Email : ajith.a@northeastern.edu ");
+
+    @Override
+    public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.button:
+                    Context context = getApplicationContext();
+                    CharSequence name = "Hi, This is Archana";
+                    CharSequence email = "Reach me at:ajith.a@northeastern.edu";
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context, name, duration);
+                    Toast toastEmail = Toast.makeText(context, email, duration);
+                    toast.show();
+                    toastEmail.show();
+                    break;
+                case R.id.button2:
+                    Intent intent = new Intent(MainActivity.this, SixButtons.class);
+                    startActivity(intent);
+                    break;
     }
+}
 }

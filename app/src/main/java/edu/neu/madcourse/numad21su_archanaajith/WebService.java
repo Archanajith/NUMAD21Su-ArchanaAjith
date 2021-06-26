9dcb68b4e10286bd3074a43941ddc10d470206d9
Archanajith <ajith.a@northeastern.edu>
2021-06-26 13:27:19 -0400

    Added basic activity setup for Assignment 6

---
+++ app/src/main/java/edu/neu/madcourse/numad21su_archanaajith/WebService.java
@@ -0,0 +1,13 @@
+package edu.neu.madcourse.numad21su_archanaajith;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WebService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_service);
+    }
+}
